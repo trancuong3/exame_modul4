db82a824fc28584eaadf36f6f77728eeed1b46da
trancuong3 <161544695+trancuong3@users.noreply.github.com>
2024-10-25 21:29:52 +0700

    Update DonHangController.java

--- src/main/java/org/example/exam4/Controller/DonHangController.java
+++ src/main/java/org/example/exam4/Controller/DonHangController.java
@@ -46,9 +46,9 @@ public class DonHangController {
         Page<DonHang> ordersPage = donHangService.getAllOrders(pageable);
 
 <<<<<<< HEAD
-        // Thiết lập model với một Page rỗng nếu không có đơn hàng nào
+      
         model.addAttribute("ordersPage", ordersPage != null ? ordersPage : Page.empty());
-        model.addAttribute("loaiSanPhams", loaiSanPhamRepository.findAll()); // Thêm danh sách loại sản phẩm nếu cần
+        model.addAttribute("loaiSanPhams", loaiSanPhamRepository.findAll()); 
 =======
        
         if (ordersPage == null || ordersPage.isEmpty()) {
